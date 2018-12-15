0ce03d4ed78cd31dc9c6f0aba85b7114777de5d8
PokestarFan <azoysarkar@gmail.com>
2018-12-15 10:56:23 -0500

    add docs

--- gendir.py
+++ gendir.py
@@ -1,6 +1,6 @@
 import inspect
 import types
-from typing import List, Union
+from typing import List, Union, Dict
 
 import Geometry
 
@@ -22,7 +22,16 @@ def scan_module(mod: types.ModuleType) -> List[types.FunctionType]:
     return funcs
 
 
-def generate_directory(mod_dir):
+def generate_directory(mod_dir: types.ModuleType) -> Dict[str, List[types.FunctionType]]:
+    """
+    Generates a dictionary where each entry is a module, and each entry contains a lit of functions retrieved from the
+    `scan_module` function.
+
+    :param mod_dir: The module package. Should be a module import that is a directory, such as Geometry.__init__
+    :type mod_dir: types.ModuleType
+    :return: The dictionary of items
+    :rtype: dict
+    """
     dictionary = {}
     for item in inspect.getmembers(mod_dir):
         if isinstance(item[1], types.ModuleType):
@@ -30,7 +39,22 @@ def generate_directory(mod_dir):
     return dictionary
 
 
-def generate_string(dict_or_list: Union[dict, list], gen_str: str = None) -> str:
+def generate_string(dict_or_list: Union[Dict[str, List[types.FunctionType]], List[types.FunctionType]],
+                    gen_str: str = None) -> str:
+    """
+    Generates the string, in markdown. It will generate a string based on the given string format.
+
+    :param dict_or_list: The dictionary or list to generate a string for. If it is a list, the gen_str does not need to
+                        have a value, as it will not be used. The format for functions are '%d. `%s`\n', where the first
+                        input is the function number, and the second input is the function name.
+    :type dict_or_list: Union[Dict[str, List[types.FunctionType]], List[types.FunctionType]]
+    :param gen_str: The generation string to use. Any given string format must have two '%s' formats included, where the
+                    first one will be the name of the module, and the second one is for the list of functions, which are
+                    numbered.
+    :type gen_str: str
+    :return: The formatted string
+    :rtype: str
+    """
     formatted_string = ''
     if isinstance(dict_or_list, list):
         count = 0
@@ -44,7 +68,11 @@ def generate_string(dict_or_list: Union[dict, list], gen_str: str = None) -> str
             formatted_string += gen_str % (name, generate_string(flist))
     return formatted_string.strip()
 
+
 if __name__ == '__main__':
+    """
+    This main function will generate the 'DIRECTORY.md' file for the module package Geometry.
+    """
     geo = generate_directory(Geometry)
 
     MODULE_FORMAT = """# %s
